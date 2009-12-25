fb8c5556ba82f496d09e1456ba647acc8b24ffa7
Stephen Jennings <sjennings@.(none)>
2009-12-24 19:26:06 -0800

    Added startup form.

---
+++ TermConfig/StartupWindow.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TermConfig
+{
+    partial class StartupWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RCButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.QSRKitchenLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.PosiwLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IPAddressLabel = new System.Windows.Forms.Label();
+            this.PositermLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CountdownTimerLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RCButton
+            // 
+            this.RCButton.Location = new System.Drawing.Point( 274, 12 );
+            this.RCButton.Name = "RCButton";
+            this.RCButton.Size = new System.Drawing.Size( 109, 62 );
+            this.RCButton.TabIndex = 0;
+            this.RCButton.Text = "RC";
+            this.RCButton.UseVisualStyleBackColor = true;
+            this.RCButton.Click += new System.EventHandler( this.RCButton_Click );
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add( this.QSRKitchenLabel );
+            this.groupBox1.Controls.Add( this.label8 );
+            this.groupBox1.Controls.Add( this.PosiwLabel );
+            this.groupBox1.Controls.Add( this.label5 );
+            this.groupBox1.Controls.Add( this.label4 );
+            this.groupBox1.Controls.Add( this.IPAddressLabel );
+            this.groupBox1.Controls.Add( this.PositermLabel );
+            this.groupBox1.Controls.Add( this.label1 );
+            this.groupBox1.Location = new System.Drawing.Point( 12, 80 );
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size( 371, 181 );
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // QSRKitchenLabel
+            // 
+            this.QSRKitchenLabel.AutoSize = true;
+            this.QSRKitchenLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.QSRKitchenLabel.Location = new System.Drawing.Point( 142, 129 );
+            this.QSRKitchenLabel.Name = "QSRKitchenLabel";
+            this.QSRKitchenLabel.Size = new System.Drawing.Size( 18, 20 );
+            this.QSRKitchenLabel.TabIndex = 7;
+            this.QSRKitchenLabel.Text = "?";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.label8.Location = new System.Drawing.Point( 6, 129 );
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size( 112, 20 );
+            this.label8.TabIndex = 6;
+            this.label8.Text = "QSR Kitchen";
+            // 
+            // PosiwLabel
+            // 
+            this.PosiwLabel.AutoSize = true;
+            this.PosiwLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.PosiwLabel.Location = new System.Drawing.Point( 142, 96 );
+            this.PosiwLabel.Name = "PosiwLabel";
+            this.PosiwLabel.Size = new System.Drawing.Size( 18, 20 );
+            this.PosiwLabel.TabIndex = 5;
+            this.PosiwLabel.Text = "?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.label5.Location = new System.Drawing.Point( 6, 64 );
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size( 79, 20 );
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Positerm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.label4.Location = new System.Drawing.Point( 6, 96 );
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size( 55, 20 );
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Posiw";
+            // 
+            // IPAddressLabel
+            // 
+            this.IPAddressLabel.AutoSize = true;
+            this.IPAddressLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.IPAddressLabel.Location = new System.Drawing.Point( 142, 33 );
+            this.IPAddressLabel.Name = "IPAddressLabel";
+            this.IPAddressLabel.Size = new System.Drawing.Size( 18, 20 );
+            this.IPAddressLabel.TabIndex = 2;
+            this.IPAddressLabel.Text = "?";
+            // 
+            // PositermLabel
+            // 
+            this.PositermLabel.AutoSize = true;
+            this.PositermLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.PositermLabel.Location = new System.Drawing.Point( 142, 64 );
+            this.PositermLabel.Name = "PositermLabel";
+            this.PositermLabel.Size = new System.Drawing.Size( 18, 20 );
+            this.PositermLabel.TabIndex = 1;
+            this.PositermLabel.Text = "?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.label1.Location = new System.Drawing.Point( 6, 33 );
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size( 97, 20 );
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // CountdownTimerLabel
+            // 
+            this.CountdownTimerLabel.AutoSize = true;
+            this.CountdownTimerLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.CountdownTimerLabel.Location = new System.Drawing.Point( 36, 24 );
+            this.CountdownTimerLabel.Name = "CountdownTimerLabel";
+            this.CountdownTimerLabel.Size = new System.Drawing.Size( 34, 20 );
+            this.CountdownTimerLabel.TabIndex = 2;
+            this.CountdownTimerLabel.Text = "4...";
+            this.CountdownTimerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // StartupWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size( 395, 270 );
+            this.Controls.Add( this.CountdownTimerLabel );
+            this.Controls.Add( this.groupBox1 );
+            this.Controls.Add( this.RCButton );
+            this.Name = "StartupWindow";
+            this.Text = "CBS Terminal Config";
+            this.Activated += new System.EventHandler( this.StartupWindow_Activated );
+            this.groupBox1.ResumeLayout( false );
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout( false );
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RCButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label QSRKitchenLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label PosiwLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label IPAddressLabel;
+        private System.Windows.Forms.Label PositermLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label CountdownTimerLabel;
+    }
+}
---
+++ TermConfig/StartupWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace TermConfig
+{
+    public partial class StartupWindow : Form
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+
+            GetIPAddress();
+            GetPosiwSetting();
+            GetQSRKitchenSetting();
+        }
+
+        private void GetIPAddress()
+        {
+            string AddressList = String.Empty;
+            var MC = new ManagementClass( "Win32_NetworkAdapterConfiguration" );
+            var collection = MC.GetInstances();
+            var addresses = new List<string>();
+
+            // Terminals have only one NIC, so this should be okay.
+            foreach ( ManagementObject obj in collection )
+            {
+                if ( (bool)( obj["IPEnabled"] ) )
+                {
+                    // This returns IPv6 addresses as well on Vista and higher,
+                    // but terminals are all XP and lower to this is okay.
+                    addresses.AddRange( (string[])( obj.GetPropertyValue( "IPAddress" ) ) );
+                }
+            }
+
+            IPAddressLabel.Text = String.Join( ", ", addresses.ToArray() );
+        }
+
+        private void GetPosiwSetting()
+        {
+            if ( File.Exists( @"C:\SC\Posiw.ini" ) )
+            {
+                var rx = new Regex( @"POSIW=(\d+)", RegexOptions.IgnoreCase );
+                var rxmatch = rx.Match( File.ReadAllText( @"C:\SC\Posiw.ini" ) );
+                if ( rxmatch.Captures.Count > 0 )
+                {
+                    PosiwLabel.Text = rxmatch.Captures[0].Value;
+                }
+            }
+        }
+
+        private void GetQSRKitchenSetting()
+        {
+        }
+
+        private void WaitThenExit()
+        {
+            Thread.Sleep( 1000 );
+            CountdownTimerLabel.Text = "3...";
+            Thread.Sleep( 1000 );
+            CountdownTimerLabel.Text = "2...";
+            Thread.Sleep( 1000 );
+            CountdownTimerLabel.Text = "1...";
+            Thread.Sleep( 1000 );
+
+            // Launch everything
+        }
+
+        private void RCButton_Click( object sender, EventArgs e )
+        {
+            var config = new TermConfigWindow();
+            config.ShowDialog();
+        }
+    }
+}
