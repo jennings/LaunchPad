7309a1cda69904067e070621d22b0d5198538a53
Stephen Jennings <sjennings@.(none)>
2009-12-24 17:26:52 -0800

    VNCLauncher now starts service or program.

--- TermConfig/Launchers/VNCLauncher.cs
+++ TermConfig/Launchers/VNCLauncher.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ServiceProcess;
+using System.IO;
+using System.Diagnostics;
 
 namespace TermConfig.Launchers
 {
@@ -10,7 +13,27 @@ namespace TermConfig.Launchers
 
         public void Launch()
         {
-            throw new NotImplementedException();
+            var service = new ServiceController( "winvnc" );
+
+            try
+            {
+                if ( service.Status == ServiceControllerStatus.Stopped )
+                {
+                    service.Start();
+                }
+            }
+            catch ( Exception )
+            {
+                if ( !File.Exists( @"C:\Program Files\UltraVNC\winvnc.exe" ) )
+                {
+                    throw new Exception( @"UltraVNC appears not to be installed in C:\Program Files\UltraVNC." );
+                }
+
+                var info = new ProcessStartInfo();
+                info.WorkingDirectory = @"C:\Program Files\UltraVNC";
+                info.FileName = @"C:\Program Files\UltraVNC\winvnc.exe";
+                Process.Start( info );
+            }
         }
 
         #endregion
