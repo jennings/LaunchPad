b898300fa56a332483d0610a408d2a1b0622daa5
Stephen Jennings <sjennings@.(none)>
2009-12-24 18:24:39 -0800

    Added SourceTerminalCopier

---
+++ TermConfig/SourceTerminalCopier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Principal;
+using System.Net;
+
+namespace TermConfig
+{
+    class SourceTerminalCopier
+    {
+        IPAddress SourceAddress;
+        string Share;
+        string Username;
+        string Password;
+        List<string> Filenames;
+
+        public bool Connected { get; set; }
+
+        public SourceTerminalCopier()
+        {
+        }
+
+        public void ConnectToRemoteShare( string address, string share, string username, string password )
+        {
+            SourceAddress = IPAddress.Parse( address );
+            Share = share;
+            Username = username;
+            Password = password;
+
+            // Connect
+        }
+
+        public void CopyFiles( List<string> filenames )
+        {
+            Filenames = filenames;
+        }
+    }
+}
