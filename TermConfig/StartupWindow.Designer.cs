8dcfaa4b412b57b2526955115ac855dc359b8fdf
Stephen Jennings <sjennings@.(none)>
2009-12-24 16:02:55 -0800

    Changed QSR to VNC monitor.

--- TermConfig/StartupWindow.Designer.cs
+++ TermConfig/StartupWindow.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.RCButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.QSRKitchenLabel = new System.Windows.Forms.Label();
+            this.VNCServerLabel = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.PosiwLabel = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -54,7 +54,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add( this.QSRKitchenLabel );
+            this.groupBox1.Controls.Add( this.VNCServerLabel );
             this.groupBox1.Controls.Add( this.label8 );
             this.groupBox1.Controls.Add( this.PosiwLabel );
             this.groupBox1.Controls.Add( this.label5 );
@@ -69,15 +69,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings";
             // 
-            // QSRKitchenLabel
+            // VNCServerLabel
             // 
-            this.QSRKitchenLabel.AutoSize = true;
-            this.QSRKitchenLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
-            this.QSRKitchenLabel.Location = new System.Drawing.Point( 142, 129 );
-            this.QSRKitchenLabel.Name = "QSRKitchenLabel";
-            this.QSRKitchenLabel.Size = new System.Drawing.Size( 18, 20 );
-            this.QSRKitchenLabel.TabIndex = 7;
-            this.QSRKitchenLabel.Text = "?";
+            this.VNCServerLabel.AutoSize = true;
+            this.VNCServerLabel.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.VNCServerLabel.Location = new System.Drawing.Point( 142, 129 );
+            this.VNCServerLabel.Name = "VNCServerLabel";
+            this.VNCServerLabel.Size = new System.Drawing.Size( 18, 20 );
+            this.VNCServerLabel.TabIndex = 7;
+            this.VNCServerLabel.Text = "?";
             // 
             // label8
             // 
@@ -85,9 +85,9 @@
             this.label8.Font = new System.Drawing.Font( "Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
             this.label8.Location = new System.Drawing.Point( 6, 129 );
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size( 112, 20 );
+            this.label8.Size = new System.Drawing.Size( 45, 20 );
             this.label8.TabIndex = 6;
-            this.label8.Text = "QSR Kitchen";
+            this.label8.Text = "VNC";
             // 
             // PosiwLabel
             // 
@@ -181,7 +181,7 @@
 
         private System.Windows.Forms.Button RCButton;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label QSRKitchenLabel;
+        private System.Windows.Forms.Label VNCServerLabel;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label PosiwLabel;
         private System.Windows.Forms.Label label5;
