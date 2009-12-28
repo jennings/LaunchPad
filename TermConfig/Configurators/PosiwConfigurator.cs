b14ed50d68671e9482882b82b0cbbca19ae9378c
Stephen Jennings <sjennings@.(none)>
2009-12-27 20:23:45 -0800

    Removed obsolete fields from PosiwConfigurator.

--- TermConfig/Configurators/PosiwConfigurator.cs
+++ TermConfig/Configurators/PosiwConfigurator.cs
@@ -9,91 +9,6 @@ namespace TermConfig.Configurators
     {
         public TerminalStation StationSettings { get; private set; }
 
-        [Obsolete]
-        string _DeviceNumberString;
-
-        [Obsolete]
-        private PosiwTerminalType _TerminalType = PosiwTerminalType.Normal;
-
-        // FIXME
-        [Obsolete]
-        public string BackofficeIP
-        { get { return "10.10.1.30"; } }
-
-        // FIXME
-        [Obsolete]
-        public string PosdriverIP
-        { get { return "10.10.1.40"; } }
-
-        // FIXME
-        [Obsolete]
-        public string BackupServerIP
-        { get { return "10.10.1.45"; } }
-
-        [Obsolete]
-        public bool BackupServer
-        {
-            get { return _TerminalType == PosiwTerminalType.BackupServer; }
-            set
-            {
-                if ( value )
-                {
-                    _TerminalType = PosiwTerminalType.BackupServer;
-                }
-                else if ( _TerminalType == PosiwTerminalType.BackupServer )
-                {
-                    _TerminalType = PosiwTerminalType.Normal;
-                }
-            }
-        }
-        [Obsolete]
-        public bool PrimaryServer
-        {
-            get { return _TerminalType == PosiwTerminalType.PrimaryServer; }
-            set
-            {
-                if ( value )
-                {
-                    _TerminalType = PosiwTerminalType.PrimaryServer;
-                }
-                else if ( _TerminalType == PosiwTerminalType.PrimaryServer )
-                {
-                    _TerminalType = PosiwTerminalType.Normal;
-                }
-            }
-        }
-        [Obsolete]
-        public bool BackoffServer
-        {
-            get { return _TerminalType == PosiwTerminalType.BackoffServer; }
-            set
-            {
-                if ( value )
-                {
-                    _TerminalType = PosiwTerminalType.BackoffServer;
-                }
-                else if ( _TerminalType == PosiwTerminalType.BackoffServer )
-                {
-                    _TerminalType = PosiwTerminalType.Normal;
-                }
-            }
-        }
-        [Obsolete]
-        public bool NormalTerminal
-        {
-            get { return _TerminalType == PosiwTerminalType.Normal; }
-            set
-            {
-                if ( value )
-                {
-                    _TerminalType = PosiwTerminalType.Normal;
-                }
-            }
-        }
-
-        
-
-
         public PosiwConfigurator( TerminalStation TerminalStationSettings )
         {
             TerminalStationSettings.Validate();
@@ -101,10 +16,6 @@ namespace TermConfig.Configurators
         }
 
         public void Configure()
-        {
-        }
-
-        public void Write()
         {
             if ( !Directory.Exists( @"C:\SC" ) ) Directory.CreateDirectory( @"C:\SC" );
             if ( !Directory.Exists( @"C:\Temp" ) ) Directory.CreateDirectory( @"C:\Temp" );
@@ -123,37 +34,37 @@ namespace TermConfig.Configurators
 
                 sw.WriteLine( @"[Startup]" );
                 sw.WriteLine( @"POSDRVR=POSDRVR-89" );
-                sw.WriteLine( @"POSIW=" + _DeviceNumberString );
-                sw.WriteLine( @"DateTimeServer=" + ( BackoffServer ? "YES" : "NO" ) );
+                sw.WriteLine( @"POSIW=" + StationSettings.DeviceNumberString );
+                sw.WriteLine( @"DateTimeServer=" + ( StationSettings.Backoffice ? "YES" : "NO" ) );
                 sw.WriteLine( @"KeyServer=NO" );
 
-                if ( PrimaryServer )
+                if ( StationSettings.PosdriverTerminal )
                 {
-                    sw.WriteLine( @"NetDriveMap1=L:\\" + BackofficeIP + @"\C" );
-                    sw.WriteLine( @"NetDriveMap2=M:\\" + BackupServerIP + @"\C" );
+                    sw.WriteLine( @"NetDriveMap1=L:\\" + StationSettings.BackofficeIPAddress + @"\C" );
+                    sw.WriteLine( @"NetDriveMap2=M:\\" + StationSettings.RedundantIPAddress + @"\C" );
                 }
-                else if ( BackupServer )
+                else if ( StationSettings.RedundantTerminal )
                 {
-                    sw.WriteLine( @"NetDriveMap1=L:\\" + PosdriverIP + @"\C" );
+                    sw.WriteLine( @"NetDriveMap1=L:\\" + StationSettings.PosdriverIPAddress + @"\C" );
                 }
-                else if ( BackoffServer )
+                else if ( StationSettings.Backoffice )
                 {
-                    sw.WriteLine( @"NetDriveMap1=L:\\" + PosdriverIP + @"\C" );
-                    sw.WriteLine( @"NetDriveMap2=M:\\" + BackupServerIP + @"\C" );
+                    sw.WriteLine( @"NetDriveMap1=L:\\" + StationSettings.PosdriverIPAddress + @"\C" );
+                    sw.WriteLine( @"NetDriveMap2=M:\\" + StationSettings.RedundantIPAddress + @"\C" );
                 }
                 else
                 {
-                    sw.WriteLine( @"NetDriveMap1=L:\\" + PosdriverIP + @"\C" );
-                    sw.WriteLine( @"NetDriveMap2=M:\\" + BackupServerIP + @"\C" );
+                    sw.WriteLine( @"NetDriveMap1=L:\\" + StationSettings.PosdriverIPAddress + @"\C" );
+                    sw.WriteLine( @"NetDriveMap2=M:\\" + StationSettings.RedundantIPAddress + @"\C" );
                 }
 
                 sw.WriteLine( @"" );
 
                 sw.WriteLine( @"[Backup]" );
                 sw.WriteLine( @"MirrorPath=L:\SC" );
-                sw.WriteLine( @"PrimaryServer=" + ( PrimaryServer ? "YES" : "NO" ) );
-                sw.WriteLine( @"BackupServer=" + ( BackupServer ? "YES" : "NO" ) );
-                sw.WriteLine( @"BackoffServer=" + ( BackoffServer ? "YES" : "NO" ) );
+                sw.WriteLine( @"PrimaryServer=" + ( StationSettings.PosdriverTerminal ? "YES" : "NO" ) );
+                sw.WriteLine( @"BackupServer=" + ( StationSettings.RedundantTerminal ? "YES" : "NO" ) );
+                sw.WriteLine( @"BackoffServer=" + ( StationSettings.Backoffice ? "YES" : "NO" ) );
                 sw.WriteLine( @"FileServer=NO" );
 
                 sw.WriteLine( @"PrimaryInterval=10" );
@@ -183,12 +94,12 @@ namespace TermConfig.Configurators
                 sw.WriteLine( @"[Network]" );
                 sw.WriteLine( @"TCP/IP=YES" );
 
-                if ( !PrimaryServer ) sw.WriteLine( @"Spcwin=" + PosdriverIP );
-                if ( !BackoffServer ) sw.WriteLine( @"BackOffice=" + BackofficeIP );
+                if ( !StationSettings.PosdriverTerminal ) sw.WriteLine( @"Spcwin=" + StationSettings.PosdriverIPAddress );
+                if ( !StationSettings.Backoffice ) sw.WriteLine( @"BackOffice=" + StationSettings.BackofficeIPAddress );
 
-                if ( !BackupServer ) sw.WriteLine( @"BackUpServer=" + BackupServerIP );
+                if ( !StationSettings.RedundantTerminal ) sw.WriteLine( @"BackUpServer=" + StationSettings.RedundantIPAddress );
 
-                if ( !BackoffServer ) sw.WriteLine( @"Posiw99=" + BackofficeIP );
+                if ( !StationSettings.Backoffice ) sw.WriteLine( @"Posiw99=" + StationSettings.BackofficeIPAddress );
                 sw.WriteLine( @"" );
 
                 sw.Flush();
