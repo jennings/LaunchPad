4927090921367bdd400272e0bbed80a631a8123c
Stephen Jennings <sjennings@.CBSINC.COM>
2009-12-28 13:52:56 -0800

    Added LaunchController.

---
+++ TermConfig/Launchers/LaunchController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TermConfig.Launchers
+{
+    class LaunchController
+    {
+        public void Launch()
+        {
+        }
+    }
+}
