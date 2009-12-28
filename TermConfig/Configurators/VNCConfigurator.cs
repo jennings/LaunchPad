5535fdee15d62d5b64e8ccce94c042e19c6a9ef5
Stephen Jennings <sjennings@.CBSINC.COM>
2009-12-28 09:10:57 -0800

    Added UltraVNC files and made VNCConfigurator work.
    - Password currently does not change.

--- TermConfig/Configurators/VNCConfigurator.cs
+++ TermConfig/Configurators/VNCConfigurator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
 
 namespace TermConfig.Configurators
 {
@@ -6,6 +9,9 @@ namespace TermConfig.Configurators
     {
         TerminalStation StationSettings;
 
+        private const string VNCDirectory = @"C:\Program Files\UltraVNC";
+        private const string VNCBackupDirectory = @"C:\Program Files\UltraVNC2";
+        private const string SourceVNCDirectory = @"Configurators\UltraVNC";
 
         public VNCConfigurator( TerminalStation settings )
         {
@@ -16,15 +22,83 @@ namespace TermConfig.Configurators
 
         public void Configure()
         {
-            // Stop VNC
+            StopVNCProcesses();
+            MoveExistingVNCDirectory();
+            CreateNewVNCDirectory();
+            ChangeVNCPassword();
+            RegisterVNCService();
+        }
 
+
+        private void StopVNCProcesses()
+        {
+            var vncprocs = Process.GetProcessesByName( "winvnc" );
+            foreach ( var proc in vncprocs )
+            {
+                proc.Kill();
+            }
+            System.Threading.Thread.Sleep( 1000 );
+        }
+
+
+        private void MoveExistingVNCDirectory()
+        {
             // Delete C:\Program Files\UltraVNC2
+            if ( Directory.Exists( VNCBackupDirectory ) )
+            {
+                Directory.Delete( VNCBackupDirectory );
+            }
 
             // Rename C:\Program Files\UltraVNC to UltraVNC2
+            if ( !Directory.Exists( VNCDirectory ) )
+            {
+                Directory.Move( VNCDirectory, VNCBackupDirectory );
+            }
+        }
 
-            // Copy good data into C:\Program Files\UltraVNC
 
-            // Change password in ultravnc.ini
+        private void CreateNewVNCDirectory()
+        {
+            CopyDirectoryRecursively( new DirectoryInfo( SourceVNCDirectory ), new DirectoryInfo( VNCDirectory ) );
+        }
+
+
+        private void ChangeVNCPassword()
+        {
+            // Hrm...
+        }
+
+
+        private void RegisterVNCService()
+        {
+            var info = new ProcessStartInfo();
+            info.FileName = @"C:\Program Files\UltraVNC\winvnc.exe";
+            info.Arguments = "-remove";
+            Process.Start( info );
+            System.Threading.Thread.Sleep( 500 );
+
+            info.Arguments = "-install";
+            Process.Start( info );
+        }
+
+
+        private void CopyDirectoryRecursively( DirectoryInfo source, DirectoryInfo dest )
+        {
+            if ( !dest.Exists )
+            {
+                dest.Create();
+            }
+
+            foreach ( var file in source.GetFiles() )
+            {
+                file.CopyTo( Path.Combine( dest.ToString(), file.Name ), true );
+            }
+
+            foreach ( var sourceSubDir in source.GetDirectories() )
+            {
+                var destSubDir = dest.CreateSubdirectory( sourceSubDir.Name );
+                CopyDirectoryRecursively( sourceSubDir, destSubDir );
+            }
         }
     }
 }
