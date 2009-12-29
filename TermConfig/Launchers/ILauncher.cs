0ac02e0d7595708f0360830edb2b2a8baad20208
Stephen Jennings <sjennings@.CBSINC.COM>
2009-12-29 06:42:56 -0800

    Added CountdownTick to StartupWindow.

--- TermConfig/Launchers/ILauncher.cs
+++ TermConfig/Launchers/ILauncher.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace TermConfig.Launchers
 {
-    interface ILauncher
+    public interface ILauncher
     {
         void Launch();
     }
---
+++ TermConfig/StartupWindow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using TermConfig.Launchers;
+
+namespace TermConfig
+{
+    public abstract class StartupWindow : Form
+    {
+        protected int SecondsBeforeClose;
+        protected Timer DelayTimer;
+        protected List<ILauncher> LaunchList;
+
+        protected void CountdownTick( object sender, EventArgs e )
+        {
+            if ( SecondsBeforeClose > 0 )
+            {
+                SecondsBeforeClose--;
+            }
+            else
+            {
+                DelayTimer.Stop();
+
+                foreach ( var launcher in LaunchList )
+                {
+                    try
+                    {
+                        launcher.Launch();
+                    }
+                    catch ( NotImplementedException )
+                    {
+                        MessageBox.Show( "Launcher not implemented: " + launcher.GetType().ToString() );
+                    }
+                    catch ( Exception ex )
+                    {
+                        MessageBox.Show( "Exception: " + launcher.GetType().ToString() + ": " + ex.Message );
+                    }
+                }
+
+                Application.Exit();
+            }
+        }
+    }
+}
