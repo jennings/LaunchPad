7cebb46216885d4705569bb9cc170e0b9e976152
Stephen Jennings <sjennings@.CBSINC.COM>
2010-01-03 19:38:29 -0800

    Added preliminary code to read in terminal names.

--- TermConfig/TerminalsReader.cs
+++ TermConfig/TerminalsReader.cs
@@ -5,6 +5,8 @@ using System.Data.OleDb;
 using ADOX;
 using System.IO;
 using System.Net;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace TermConfig
 {
@@ -24,12 +26,12 @@ namespace TermConfig
             }
         }
 
+        public bool Integrous { get; private set; }
+        public List<TerminalStation> Terminals { get; set; }
+
         private const string Filename = @"Terminals.mdb";
         private OleDbConnection Db;
 
-        public bool Integrous { get; private set; }
-
-        public List<TerminalStation> Terminals { get; set; }
 
         private TerminalsReader()
         {
@@ -115,13 +117,96 @@ namespace TermConfig
 
         void RefreshTerminalList()
         {
-            // Run posidbfw on the posdriver
+            // Map L: to posdriver
+            var netExecutable = Path.Combine(
+                Environment.GetFolderPath( Environment.SpecialFolder.System ),
+                "net.exe" );
 
-            // Read NAMETERM.DBF
+            var posdriverCFolder = String.Format(
+                @"\\{0}\C",
+                SettingsReader.Instance.PosdriverIPAddress.ToString() );
 
-            // Read SPCWIN.INI
+            var info = new ProcessStartInfo();
+            info.Arguments = @"use L: " + posdriverCFolder;
+            info.FileName = netExecutable;
 
-            // Add terminals to Terminals.mdb
+            if ( !File.Exists( netExecutable ) )
+            {
+                throw new Exception( @"net.exe does not exist in system/system32 folder." );
+            }
+
+            var netProcess = Process.Start( info );
+
+            if ( netProcess.WaitForExit( 15000 ) )
+            {
+            }
+            else
+            {
+                throw new Exception( @"NET USE did not exit within 15 seconds." );
+            }
+
+            // Run "posidbfw /dbf mm/dd/yy" on the posdriver
+            var posidbfwinfo = new ProcessStartInfo();
+            posidbfwinfo.FileName = @"L:\SC\Posidbfw.exe";
+            posidbfwinfo.WorkingDirectory = @"L:\SC";
+            posidbfwinfo.Arguments = @"/dbf " + DateTime.Now.AddDays( -1 ).ToString( "MM/dd/yy" );
+            var posidbfw = Process.Start( posidbfwinfo );
+            posidbfw.WaitForExit();
+
+            if ( !Directory.Exists( @"C:\Temp" ) )
+            {
+                Directory.CreateDirectory( @"C:\Temp" );
+            }
+            File.Copy( @"L:\DBF\NAMETERM.DBF", @"C:\Temp\NAMETERM.DBF", true );
+            File.Copy( @"L:\SC\SPCWIN.INI", @"C:\Temp\SPCWIN.INI", true );
+
+            // Add Nameterm terminals to tblTerminals
+
+            var tempTerminals = new List<TerminalStation>();
+
+            var dbfdb = new OleDbConnection( @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=C:\Temp; Extended Properties=""dBase IV""" );
+            dbfdb.Open();
+
+            using ( var dbfcmd = new OleDbCommand() )
+            {
+                dbfcmd.Connection = dbfdb;
+                dbfcmd.CommandText = @"SELECT [Code], [Name] FROM nameterm;";
+                var dbfreader = dbfcmd.ExecuteReader();
+
+                while ( dbfreader.Read() )
+                {
+                    tempTerminals.Add( new TerminalStation( Convert.ToInt32( dbfreader["Code"] ), dbfreader["Name"].ToString() ) );
+                }
+            }
+
+            dbfdb.Close();
+
+            // Add IP addresses to each terminal
+
+            foreach ( var terminal in tempTerminals )
+            {
+                // Regex c:\temp\spcwin.ini - this is terrible
+                var rx = new Regex( @"^Device" + terminal.DeviceNumber.ToString( "D" ) + @"=(\S+)" );
+                var ipstring = rx.Replace( File.ReadAllText( @"C:\Temp\spcwin.ini" ), "$1" );
+                if ( ipstring != null )
+                {
+                    terminal.IPAddress = IPAddress.Parse( ipstring );
+                }
+            }
+
+            Db.Open();
+            using ( var cmd = new OleDbCommand( @"INSERT INTO tblTerminals ([devicenum],[name],[ipaddress]) VALUES (?,?,?);", Db ) )
+            {
+                foreach ( var terminal in tempTerminals )
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.Add( new OleDbParameter( @"DevNum", terminal.DeviceNumber.ToString() ) );
+                    cmd.Parameters.Add( new OleDbParameter( @"Name", terminal.Name ) );
+                    cmd.Parameters.Add( new OleDbParameter( @"IPAddress", terminal.IPAddress.ToString() ) );
+
+                }
+            }
+            Db.Close();
         }
     }
 
@@ -136,6 +221,12 @@ namespace TermConfig
             DeviceNumber = deviceNumber;
             Name = name;
             IPAddress = ipaddress;
+        }
+
+        public TerminalStation( int deviceNumber, string name )
+        {
+            DeviceNumber = deviceNumber;
+            Name = name;
         }
     }
 }
