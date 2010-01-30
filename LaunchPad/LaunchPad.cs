12c2175180d3944fd7a247c6da7cb2cab078c4fd
Stephen Jennings <sjennings@.CBSINC.COM>
2010-01-29 16:09:38 -0800

    Created ability to run as service with -s or /s switch.

---
+++ LaunchPad/LaunchPad.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Windows.Forms;
+using LaunchPad.Forms;
+
+namespace LaunchPad
+{
+    static class LaunchPad
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main( string[] args )
+        {
+            if ( args.Length > 0 && ( args[0].Equals( "-s" ) || args[0].Equals( "/s" ) ) )
+            {
+                // Run as service
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] { new LaunchPadService() };
+                ServiceBase.Run( ServicesToRun );
+            }
+            else
+            {
+                // Run front-end application
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault( false );
+
+                var settings = SettingsReader.Instance;
+
+                switch ( settings.PointOfSale )
+                {
+                    case PointOfSale.Positouch:
+                        if ( settings.Integrous )
+                        {
+                            Application.Run( new PositouchStartupWindow() );
+                        }
+                        else
+                        {
+                            Application.Run( new PositouchInitialConfigWindow() );
+                        }
+                        break;
+
+                    case PointOfSale.Aloha:
+                        if ( settings.Integrous )
+                        {
+                            Application.Run( new AlohaStartupWindow() );
+                        }
+                        else
+                        {
+                            Application.Run( new AlohaInitialConfigWindow() );
+                        }
+                        break;
+
+                    case PointOfSale.None:
+                    default:
+                        MessageBox.Show( @"Add POSITOUCH or ALOHA flag file to use." );
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ LaunchPad/LaunchPadService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace LaunchPad
+{
+    class LaunchPadService : ServiceBase
+    {
+        public static readonly string WorkingDirectory;
+        public static readonly string LaunchPadDirectory;
+        public static readonly string LogFilePath;
+
+        static LaunchPadService()
+        {
+            WorkingDirectory = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location );
+            LaunchPadDirectory = @"C:\LaunchPad";
+            LogFilePath = Path.Combine( LaunchPadDirectory, @"LaunchPadService.log" );
+        }
+
+        protected override void OnStart( string[] args )
+        {
+            var runFrom = Assembly.GetExecutingAssembly().Location;
+            Directory.GetParent( runFrom );
+            System.IO.File.WriteAllText( LogFilePath, "Last run at: " + DateTime.Now );
+        }
+    }
+}
