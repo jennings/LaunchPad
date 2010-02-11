00f942e26e615fb8a6c0514d475f9f78e5189872
Stephen Jennings <sjennings@.CBSINC.COM>
2010-02-10 18:16:58 -0800

    Added PositouchTerminalSelectionModel.

---
+++ LaunchPad/Models/PositouchTerminalSelectionModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace LaunchPad.Models
+{
+    public class PositouchTerminalSelectionModel
+    {
+        public IPAddress @IPAddress { get; set; }
+        public int DeviceNumber { get; set; }
+        public PosiwTerminalType Type { get; set; }
+    }
+}
