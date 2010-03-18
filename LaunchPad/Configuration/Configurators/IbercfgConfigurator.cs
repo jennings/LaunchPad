1419cfa153eab7da4f9f2f38614939d6701b10f5
Stephen Jennings <sjennings@cbs-posi.com>
2010-03-18 15:38:52 -0700

    Added IbercfgConfigurator.

---
+++ LaunchPad/Configuration/Configurators/IbercfgConfigurator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using LaunchPad.Configuration.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LaunchPad.Configuration.Configurators
+{
+    class IbercfgConfigurator : IConfigurator
+    {
+        public bool RequiresElevation { get { return false; } }
+        public bool RequiresAuthentication { get { return false; } }
+
+        private Dictionary<string, string> Variables;
+
+        private IbercfgConfigurator() { }
+        public IbercfgConfigurator( IbercfgTask task )
+        {
+            Variables = new Dictionary<string, string>();
+            Variables.Add( "TERM", task.TerminalNumber.ToString() );
+            Variables.Add( "SERVER", "TERM" + task.TerminalNumber );
+            Variables.Add( "NUMTERMS", task.NumberTerminals.ToString() );
+            Variables.Add( "TERMSTR", "TERM" );
+            Variables.Add( "IBERROOT", "Aloha" );
+            Variables.Add( "LOCALDIR", @"C:\%IBERROOT%" );
+            Variables.Add( "ROBUST", "TRUE" );
+            Variables.Add( "MASTERCAPABLE", task.MasterCapable ? "TRUE" : "FALSE" );
+            Variables.Add( "SERVERCAPABLE", task.ServerCapable ? "TRUE" : "FALSE" );
+            Variables.Add( "AUTOEXIT", "TRUE" );
+            Variables.Add( "EDCPATH", @"\\%SERVER%\BOOTDRV\%IBERROOT%\EDC" );
+        }
+
+        public void Configure()
+        {
+            string ibercfg = File.ReadAllText( @"C:\IBERCFG.BAT" );
+
+            foreach ( var val in Variables )
+            {
+                ibercfg = UpdateIbercfg( ibercfg, val.Key, val.Value );
+            }
+
+            File.WriteAllText( @"C:\IBERCFG.BAT", ibercfg );
+        }
+
+        private string UpdateIbercfg( string ibercfg, string variable, string value )
+        {
+            var rx = new Regex( @"^(SET (" + variable + ")=(.*))$", RegexOptions.IgnoreCase | RegexOptions.Multiline );
+            return rx.Replace( ibercfg, @"SET $2=$3" );
+        }
+    }
+}
---
+++ LaunchPad/Configuration/Tasks/IbercfgTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LaunchPad.Configuration.Tasks
+{
+    [Serializable]
+    public class IbercfgTask : ITask
+    {
+        public int TerminalNumber { get; private set; }
+        public int NumberTerminals { get; private set; }
+        public string FileserverName { get; private set; }
+        public bool MasterCapable { get; private set; }
+        public bool ServerCapable { get; private set; }
+
+        private IbercfgTask() { }
+        public IbercfgTask( int terminalNumber, int countTerminals, string fileserverName, bool masterCapable, bool serverCapable )
+        {
+            TerminalNumber = terminalNumber;
+            NumberTerminals = countTerminals;
+            FileserverName = fileserverName;
+            MasterCapable = masterCapable;
+            ServerCapable = serverCapable;
+        }
+    }
+}
